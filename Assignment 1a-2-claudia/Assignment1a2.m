ef902d560e12fea421bd9ec083baff18e8659cb4
Claudia Raducanu <claudia.m.raducanu@gmail.com>
2018-03-10 22:22:00 +0100

    It1 var cont

--- Assignment 1a-2-claudia/Assignment1a2.m
+++ Assignment 1a-2-claudia/Assignment1a2.m
@@ -8,7 +8,7 @@ clear all
 %%  Determine input
 %   Select input file and sheet
 
-input      =  '/Input_AE4424_Ass1Verification.xlsx';
+input      =  'Input_AE4424_Ass1Verification.xlsx';
 %filsol      =   'Solutions.xlsx';
 
 %% Inputs
@@ -64,7 +64,7 @@ input      =  '/Input_AE4424_Ass1Verification.xlsx';
     model                 =   'Initial';  % name of model
     RMP                   =    Cplex(model); % define the new model
     RMP.Model.sense       =   'minimize';
-
+    
     %   Decision variables
     DV                      =  K+A;  % Number of Decision Var (xijk)
 
@@ -75,7 +75,7 @@ input      =  '/Input_AE4424_Ass1Verification.xlsx';
     obj                     =   [c_p; M] ;
     lb                      =   zeros(DV, 1);                                 %Lower bounds
     ub                      =   inf(DV, 1);                                   %Upper bounds
-    ctype                   =   char(ones(1, (DV)) * ('I'));                  %Variable types 'C'=continuous; 'I'=integer; 'B'=binary
+    %ctype                   =   char(ones(1, (DV)) * ('I'));                  %Variable types 'C'=continuous; 'I'=integer; 'B'=binary
 
 l = 1;
 for k = 1:K
@@ -91,36 +91,39 @@ for a = 1:A
     %end
 end 
 % cplex.addCols(obj,A,lb,ub,ctype,name)  http://www-01.ibm.com/support/knowledgecenter/#!/SSSA5P_12.2.0/ilog.odms.cplex.help/Content/Optimization/Documentation/CPLEX/_pubskel/CPLEX1213.html
-    RMP.addCols(obj, [], lb, ub, ctype, NameDV);
+    RMP.addCols(obj, [], lb, ub);
 
 %%  Constraints
-% 1. Bundle constraint
+% 1. Commodity constraint
     for k = 1:K
         C1 = zeros(1,DV);
         C1(Findex(k)) = 1;   
-        RMP.addRows(1, C1, 1,sprintf('Bundle_%d',k));
+        RMP.addRows(1, C1, 1,sprintf('Commodity_%d',k));
     end
 % 
-% % 2. Capacity constraint
+% 2. Bundle constraint
     for a = 1:A
         C2 = zeros(1,DV);
         for k = 1:K
             C2(Findex(k)) = demand(k)*d_ij_p(k,a);
         end
         C2(Sindex(a,K)) = -1; 
-        RMP.addRows(0, C2, capacity(a),sprintf('Capacity_Constraint_%d',a));
+        RMP.addRows(0, C2, capacity(a),sprintf('Bundle_%d',a));
     end
 
 %%  Execute model
     RMP.Param.mip.limits.nodes.Cur    = 1e+8;        %max number of nodes to be visited (kind of max iterations)
     RMP.Param.timelimit.Cur           = 120;         %max time in seconds
-    RMP.Param.solutiontype.Cur        = 2;
 % %   Run CPLEX
+    
     RMP.solve();
     RMP.writeModel([model '.lp']);
-    RMP.findprop('Solution').delete
     
-
+    pi_ij = RMP.Solution.dual; % dual variables of bundle constraints
+          =  RMP.Solution.x;
+    
+    
+    
 %Function to return index of decision variables
 function out = Findex(k)
     out =  k;  % Function given the variable index for each X(i,j,k) [=(m,n,p)]  
