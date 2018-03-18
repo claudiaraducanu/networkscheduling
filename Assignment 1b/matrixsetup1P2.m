d77336c1bbdd06309bf44c5504f5479509c70929
luckiluka@gmail.com <luckiluka@gmail.com>
2018-03-18 09:26:03 +0100

    matrixsetup Ass1P2

--- Assignment 1b/matrixsetup1P2.m
+++ Assignment 1b/matrixsetup1P2.m
@@ -1,10 +1,40 @@
-%function [nodes, fare, demand , capacity, origin , dest, recapture, s, ...
-%           t] = matrixsetup(filename)
-       %% Read file for network
+% %function [nodes, fare, demand , capacity, origin , dest, recapture, s, ...
+% %           t] = matrixsetup(filename)
+%        %% Read file for network
+%    
+%     [~, ditinerary.origin]  = xlsread(filename,2,'B2:B738'); % names of departure airports
+%     [~, ditinerary.dest]    = xlsread(filename,2,'C2:C738'); % names of arrival airports
+%     [~, ditinerary.flightnr]= xlsread(filename,2,'F2:F738'); % flight number Leg 1
+%     ditinerary.fare         = xlsread(filename,2,'E2:E738'); % fare associated to all arcs
+%     ditinerary.demand       = xlsread(filename,2,'D2:D738'); % demand associated to all arcs
+%     
+%     [~, dflight.origin]  = xlsread(filename,2,'B2:B738'); % names of departure airports
+%     [~, dflight.dest]    = xlsread(filename,2,'C2:C738'); % names of arrival airports
+%     [~, dflight.flightnr]= xlsread(filename,2,'F2:F738'); % flight number 
+%     dflight.capacity     = xlsread(filename,1,'F2:F233'); % capacity associated to existing arc
+%     
+%     dpath.recapture     = xlsread(filename,3,'C2:C300'); % recapture rate associated to some arcs
+%        %% Trying to figure it out
+%     % itenaries
+%     itinerary.data.origin     = ditinerary.origin;
+%     itinerary.data.dest       = ditinerary.dest;
+%     itinerary.data.flightnr   = ditinerary.flightnr; %How to do it, 2 flight legs?
+%     itinerary.data.fare       = ditinerary.fare;
+%     itinerary.data.demand     = ditinerary.demand;
+%     
+%     flight.data.origin      = dflight.origin;
+%     flight.data.dest        = dflight.dest;
+%     flight.data.flightnr    = dflight.flightnr;
+    
    
-    [~, dnetwork.originP]  = xlsread(filename,2,'B2:B738'); % names of departure airports
-    [~, dnetwork.destP]    = xlsread(filename,2,'C2:C738'); % names of arrival airports
-    dnetwork.fare          = xlsread(filename,2,'E2:E738'); % fare associated to all arcs
-    dnetwork.demand        = xlsread(filename,2,'D2:D738'); % demand associated to all arcs
-    dnetwork.capacity      = xlsread(filename,1,'F2:F233'); % capacity associated to existing arc
-    dnetwork.recapture     = xlsread(filename,3,'C2:C300'); % recapture rate associated to some arcs
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
