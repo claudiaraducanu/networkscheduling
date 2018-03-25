ca4c10fad7c98781f6a497e12179ef7eadf05d5d
luckiluka@gmail.com <luckiluka@gmail.com>
2018-03-25 22:24:04 +0200

    struggeling

--- Assignment 1P2/assignment1P2.m
+++ Assignment 1P2/assignment1P2.m
@@ -15,6 +15,7 @@ input      =  'Input_AE4424_Ass1P2.xlsx';
 
 not_opt_col = 1;
 not_opt_row = 1;
+colz = [];
 rowz = [];
 
 titer =0;
@@ -62,11 +63,15 @@ titer =0;
     %%  Execute model 
     %%
     sol = RMP.solve();
-    ObVals = [ObjVals;RMP.Solution.objval];
+    ObjVals = [];
+    ObjVals = [ObjVals;RMP.Solution.objval];
     primal  = RMP.Solution.x;   
-    [find(primal),primal(find(primal))]
+    [find(primal),primal(find(primal))];
     dual    = RMP.Solution.dual;
     %RMP.writeModel([model '.lp']);
+    
+    
+    
     
     
 
@@ -86,14 +91,17 @@ while not_opt_col == 1 || not_opt_row == 1
 %%
 iter=0;
    while not_opt_col == 1
-   %% Objective function
+   %% Iterations
+   %%
+   testlist = [];
    iter = iter +1;
     disp('-------------------------------------------------');
     disp(['Column iteration: ',num2str(iter)]);   
     disp('-------------------------------------------------');
     
-    %   Get dual variables
-    
+   %% Pricing Problem
+   %%
+   % Get dual variables 
     pi   = dual(1:L);                 % dual variables of capacity constraints (slack)
     sigma = zeros(P,1);
     if size(dual) > L
@@ -102,111 +110,145 @@ iter=0;
         end
     end
     
-        %% Pricing Problem
     checkcol = 0; 
     R = P;
     for p = 1:P
         for r = 1:R
+            
             pi_j = 0;
             pi_i = 0;
+            
             for i = 1:L
                 pi_i = pi_i + pi(i)*delta{p,1}(i);
                 pi_j = pi_j + pi(i)*delta{r,1}(i);    
             end
+            
+            
             if (fare(p) - pi_i) - (Bpr(p,r)*(fare(r) - pi_j )) - sigma(p) < 0 
                 D = find(r==col(:,2));
-                if isempty(D)  
+                Dp = find(col(D,1)==p);
+                
+                if isempty(Dp) == 1 || isempty(D) == 1
                     checkcol = checkcol + 1;
-                    col = [col; [p r]]; 
-                elseif p ~= col(D,1) 
-                    checkcol = checkcol + 1;
-                    col = [col; [p r]]; 
+                    
+                    colz = [colz, [p r]]; % only the added columns
+                    col = [col; [p r]];   % all columns
+                    
+                    cost = [cost;costfull(col(p,1),col(p,2))];
+                    
+                    % New objective function
+                    A       = RMP.Model.A(:,p);
+                    
+                    % add a constraint to every L if the leg is used in p.
+                    deltasp = find(delta{p,1}(:)~=0);
+                    if isempty(deltasp) ~= 0 
+                        A(deltasp)= 1;
+                    end
+                    
+                    % substract rr to every L if the leg is used in r.
+                    deltasr = find(delta{r,1}(:)~=0);
+                    if deltasr
+                        A(deltasr)= -Bpr(p,r); 
+                    end
+                    
+                    obj     = costfull(col(p,1),col(p,2));
+                    lb      = [0];
+                    ub      = [inf];
+                    ctype   = [];
+
+                    RMP.addCols(obj,A,lb,ub);
+
                 end
             end
         end
     end
     
+    RMP.solve();
+    ObjVals = [ObjVals;RMP.Solution.objval];
+    dual = RMP.Solution.dual;
+    pi   = dual(1:L);                 % dual variables of capacity constraints (slack)
+    sigma = zeros(P,1);
+    if size(dual) > L
+        for ro = 1:numel(rowz)
+            sigma(rowz(ro))  = dual(L+ro);
+        end
+    end
+
+    
     if checkcol == 0
         not_opt_col = 0;
     else 
         not_opt_col = 1;
-        not_opt_row = 1;
+        not_opt_row = 1;       
     end    
+    
+    %% Adding paths and rows to A matrix
+%     A = zeros(size(RMP.Model.A,1),1);
+%  
+%     % Indices of paths
+%     A(first_nr_from) = 1;
+%     if second_nr_from ~= 0
+%         A(second_nr_from) = 1;
+%     end
+% 
+% 
+%     A(first_nr_to) = -rr;
+%     if second_nr_to ~= 0
+%         A(second_nr_to) = -rr;
+%     end
+% 
+%     %add the correct index in the A matrix for the added
+%     %seperation problem rows
+% 
+% 
+%     if sum(from == added_rows) > 0
+%         indices = find(from == added_rows);
+%         A(232+indices) = 1;
+%     end
+
     
    end
    
   
  
 %% ROW GENERATION
+%%
      iteration = 0;
    while not_opt_row == 1
   %%  Initiate CPLEX model
   iteration = iteration +1;
-        %   Create model 
-        model                 =   'Initial';  % name of model
-        RMP                   =    Cplex(model); % define the new model
-        RMP.Model.sense       =   'minimize';
-   %% Objective function
-        DV                      =  numel(col(:,1));
-   
-        cost = zeros(numel(col(:,1)),1);
-        for p = 1:numel(col(:,1))    
-            cost(p) = costfull(col(p,1),col(p,2));                % select only costs you need
-        end
-
-        obj                     =   cost ;
-        lb                      =   zeros(DV, 1);                                 %Lower bounds
-        ub                      =   inf(DV, 1);                                   %Upper bounds             
-
-        RMP.addCols(obj, [], lb, ub);
-        
-   %%  Constraints
-    % 1. Capacity constraint
-        for i = 1:L
-            C11 = zeros(1,DV);
-            C12 = zeros(1,DV);
-            for pr = 1:DV
-                if delta{col(pr,1),1}(i) ~= 0 
-                    C11(pr)= 1;
-                end
-                if delta{col(pr,2),1}(i) ~= 0 && ismember(col(pr,2), col(:,1))
-                    C12(pr) = Bpr(col(pr,1),col(pr,2)); 
-                end
-            end
-            C1 = C11 - C12;
-            RMP.addRows(Q(i)-capacity(i), C1, inf, sprintf('Capacity_%03d',i));
-        end
-           
-        
-       %%  Execute model 
-    %   Run CPLEX
-    RMP.solve();
-    RMP.writeModel([model '.lp']);
-    
-    if RMP.Solution.status == 1 
-        primal_feasibility = 1; 
-    end
-    
-%     disp('-------------------------------------------------');
-%     disp(['Primal Feasibility: ',num2str(primal_feasibility)]);  
-    
-    %   Get dual variables
-    primal  = RMP.Solution.x;   
-    dual    = RMP.Solution.dual;
+  
+  primal = RMP.Solution.x;
     
         %% Separation Problem
     % 2. Demand constraint
     check = 0;
     for p = 1:P
         B = find(col(:,1)==p);
-        if sum(primal(B)) > demand(p) && sum(p~=rowz) == numel(rowz)
-            check = check + 1;
-            C = zeros(1,DV);
-            C(B) = 1;
-            RMP.addRows(-inf, C, demand(p), sprintf('Demand_%03d',p));
-            rowz = [rowz;p];
+        
+        if sum(primal(B)) > demand(p)
+            D = find(p==row(:,1));
+            
+            if isempty(D) == 1
+                check = check + 1;
+                C = zeros(1,DV);
+                C(B) = 1;
+                RMP.addRows(-inf, C, demand(p), sprintf('Demand_%03d',p));
+                rowz = [rowz;p];
+            end
         end
     end 
+    
+           %%  Execute model 
+    %   Run CPLEX
+    RMP.solve();
+    ObjVals = [ObjVals;RMP.Solution.objval];
+
+    
+%     %   Get dual variables
+%     primal  = RMP.Solution.x;   
+%     dual    = RMP.Solution.dual;
+    
     
     if check == 0
         not_opt_row = 0;
@@ -221,14 +263,11 @@ iter=0;
     
  end
     
-        %%  Function to return index of decision variables
-function out = Tindex(p)
-        out = p;
-end
-   
-        
-
-        
+%         %%  Function to return index of decision variables
+% function out = Tindex(p)
+%         out = p;
+% end
+%
 %     % 2. Demand constraint
 %         if isempty(rowz)==0
 %             for ro = 1:numel(rowz)
