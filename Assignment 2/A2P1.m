d07003ea875eafa2c29eb265ff5367551a669d8c
luckiluka@gmail.com <luckiluka@gmail.com>
2018-04-05 17:10:23 +0200

    copying row and column to ass2

---
+++ Assignment 2/A2P1.m
@@ -0,0 +1,288 @@
+%%  Initialization
+% Claudia Raducanu and Luka Van de Sype
+addpath('C:\Program Files\IBM\ILOG\CPLEX_Studio1271\cplex\matlab\x64_win64'); %Luka
+% clc
+ clearvars
+% clear all
+
+%%  Determine input
+input      =  'Input_AE4424_Ass1P2.xlsx';
+
+% Inputs
+[P, R, L, fare, fare_r, demand, capacity, col, delta, Q, costfull, Bpr, ... 
+     recap_p, recap_r, recaprate] = matrixsetup1P2(input) ;
+ 
+% input      =  'Input_Example.xlsx';
+% [P, R, L, fare, fare_r, demand, capacity, col, delta, Q, costfull, Bpr, ... 
+%        recap_p, recap_r, recaprate] = matrixsetup1P2_EXAMPLE(input) ;
+
+not_opt_col = 1;
+not_opt_row = 1;
+colz = [];
+rowz = [];
+
+titer =0;
+
+ %%  Initiate CPLEX model
+ %%
+        %   Create model 
+        model                 =   'Model';  % name of model
+        RMP                   =    Cplex(model); % define the new model
+        RMP.Model.sense       =   'minimize';
+
+
+        %   Decision variables
+        DV                      =  numel(col(:,1));
+   
+        cost = zeros(numel(col(:,1)),1);
+        for p = 1:numel(col(:,1))    
+            cost(p) = costfull(col(p,1),col(p,2));                % select only costs you need
+        end
+
+        obj                     =   cost ;
+        lb                      =   zeros(DV, 1);                                 %Lower bounds
+        ub                      =   inf(DV, 1);                                   %Upper bounds             
+        
+        RMP.addCols(obj, [], lb, ub);
+        
+   %%  Constraints
+   %%
+    % 1. Capacity constraint
+        for i = 1:L
+            C11 = zeros(1,DV);
+            C12 = zeros(1,DV);
+            for pr = 1:DV
+                if delta{col(pr,1),1}(i) ~= 0 
+                    C11(pr)= 1;
+                end
+                if delta{col(pr,2),1}(i) ~= 0 && ismember(col(pr,2), col(:,1))
+                    C12(pr) = Bpr(col(pr,1),col(pr,2)); 
+                end
+            end
+            C1 = C11 - C12;
+            RMP.addRows(Q(i)-capacity(i), C1, inf, sprintf('Capacity_%d',i));
+        end
+
+    %%  Execute model 
+    %%
+    sol = RMP.solve();
+    OV = [];
+    OV = [OV;RMP.Solution.objval];
+    primal  = RMP.Solution.x;   
+    dual    = RMP.Solution.dual;
+    RMP.writeModel([model '.lp']);
+    
+
+    
+    
+    
+    
+    Alist = [RMP.Model.A];
+    
+
+%% SUPER BIG LOOP
+%%
+while not_opt_col == 1 || not_opt_row == 1
+
+    titer = titer +1;
+    disp('-------------------------------------------------');
+    disp(['Iteration: ',num2str(titer)]);   
+    disp('-------------------------------------------------');
+    
+    dual    = RMP.Solution.dual;
+    
+    
+%% COLUMN GENERATION
+%%
+iter=0;
+   while not_opt_col == 1
+   %% Iterations
+   %%
+   iter = iter +1;
+    disp('-------------------------------------------------');
+    disp(['Column iteration: ',num2str(iter)]);   
+    disp('-------------------------------------------------');
+    
+   %% Pricing Problem
+   %%
+   % Get dual variables 
+    pi   = dual(1:L);                 % dual variables of capacity constraints (slack)
+    sigma = zeros(P,1);
+    if size(dual) > L
+        for ro = 1:numel(rowz)
+            sigma(rowz(ro)) = dual(L+ro);
+        end
+    end
+    
+    checkcol = 0; 
+    R = P;
+    
+    for re = 1:numel(recap_p) % run only for those that can be recaptured
+            pi_j = 0;
+            pi_i = 0;
+            
+            for i = 1:L
+                if delta{recap_p(re),1}(i) ~= 0     % search for the flight legs for each recap_p
+                    pi_i = pi_i + pi(i);            % for those flight legs, add pi
+                end
+                if delta{recap_r(re),1}(i) ~= 0
+                    pi_j = pi_j + pi(i);   
+                end
+            end
+            
+% recap_p is the path p that can be recaptured by path r in recap_r            
+             if (fare(recap_p(re)) - pi_i) - (Bpr(recap_p(re),recap_r(re))*(fare(recap_r(re)) - pi_j )) - sigma(recap_p(re)) < 0 
+                D = find(recap_r(re)==col(:,2));
+                Dp = find(col(D,1)==recap_p(re));
+                
+                if isempty(Dp) == 1
+                    checkcol = checkcol + 1; % to check if columns are added (loop stop)
+                    
+                    colz = [colz; [recap_p(re) recap_r(re)]]; % only the added columns
+                    col = [col; [recap_p(re) recap_r(re)]];   % all columns
+                    
+                    cost = [cost;costfull(col(recap_p(re),1),col(recap_p(re),2))];
+                    
+                    % New objective function
+                    A       = RMP.Model.A(:,recap_p(re));
+                    
+                    % add a constraint to every L if the leg is used in p.
+                    deltasp = 0;
+                    deltasp = find(delta{recap_p(re),1}(:)~=0); % find 
+                    if isempty(deltasp) == 0        % if the list is not 0, add a constraint
+                         A(deltasp)= 1;
+                    end
+                    
+                    % substract rr to every L if the leg is used in r.
+                    deltasr = 0;
+                    deltasr = find(delta{recap_r(re),1}(:)~=0);
+                    if isempty(deltasr) == 0        % if the list is not 0, add a constraint
+                        A(deltasr)= A(deltasr) - Bpr(recap_p(re),recap_r(re)); 
+                    end
+                    
+                    %Alist   = [Alist,A];
+                    
+                    obj     = costfull(recap_p(re),recap_r(re));
+                    lb      = [0];
+                    ub      = [inf];
+                    %ctype   = []; 
+
+                    RMP.addCols(obj,A,lb,ub);
+
+                end
+            end
+    end
+    
+     
+    RMP.solve();
+    OV = [OV;RMP.Solution.objval];
+    RMP.writeModel([model '.lp']);
+    
+    dual = RMP.Solution.dual;
+    pi   = dual(1:L);                 % dual variables of capacity constraints (slack)
+    sigma = zeros(P,1);
+    if size(dual) > L
+        for ro = 1:numel(rowz)
+            sigma(rowz(ro))  = dual(L+ro);
+        end
+    end
+
+    
+    
+    if checkcol == 0
+        not_opt_col = 0;
+    else 
+        not_opt_col = 1;
+        not_opt_row = 1;       
+    end    
+    
+   end
+     
+ 
+%% ROW GENERATION
+%%
+     iteration = 0;
+
+   while not_opt_row == 1
+  %%  Initiate CPLEX model
+  iteration = iteration +1;
+
+    disp('-------------------------------------------------');
+    disp(['Row iteration: ',num2str(iteration)]);   
+    disp('-------------------------------------------------');
+    size(rowz)
+  
+  DV     =  numel(col(:,1));
+  primal = RMP.Solution.x;
+     
+        %% Separation Problem
+    % 2. Demand constraint
+    check = 0;
+    for p = 1:P
+        B = find(col(:,1)==p);
+        
+        if sum(primal(B)) > demand(p)
+            D = find(p==rowz);
+            
+            if isempty(D) == 1
+                check = check + 1;
+                C = zeros(1,DV);
+                C(B) = 1;
+                RMP.addRows(-inf, C, demand(p), sprintf('Demand_%03d',p));
+                rowz = [rowz;p];
+            end
+        end
+    end 
+
+    
+           %%  Execute model 
+    %   Run CPLEX
+    RMP.solve();
+    OV = [OV;RMP.Solution.objval];
+
+    
+%     %   Get dual variables
+%     primal  = RMP.Solution.x;   
+%     dual    = RMP.Solution.dual;
+    
+    
+    if check == 0
+        not_opt_row = 0;
+    else
+        not_opt_row = 1;
+        not_opt_col = 1;
+    end       
+        
+   end
+    
+end
+
+%% Post Processing
+%%
+%% Spilled Passengers
+%%
+spilled = zeros(L,1);
+for i = 1:L
+    gotspill = zeros(DV,1);
+    for pr = 1:DV
+       gotspill = RMP.Solution.x(col(pr,2));   
+    end
+    spilled(i) = Q(i)-capacity(i)+gotspill;
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assignment 2/matrixsetup2P1.m
@@ -0,0 +1,126 @@
+%function [nodes, fare, demand , capacity, origin , dest, recapture, s, ...
+%           t] = matrixsetup(filename)
+       %% Read file for network
+
+    % Not used
+%     [~, ditinerary.origin]  = xlsread(filename,2,'B2:B738'); % names of departure airports
+%     [~, ditinerary.dest]    = xlsread(filename,2,'C2:C738'); % names of arrival airports   
+
+%     [~, dflight.origin]  = xlsread(filename,2,'B2:B738'); % names of departure airports
+%     [~, dflight.dest]    = xlsread(filename,2,'C2:C738'); % names of arrival airports    
+   
+    
+        %% EXAMPLE Input
+
+%    clearvars
+%    clear all
+%    filename = 'Input_AE4424_Ass1P2.xlsx';
+
+        %% EXAMPLE Input
+function [P, R, L, fare, fare_r, demand, capacity, col, delta, Q, costfull, Bpr, ... 
+          recap_p, recap_r, recaprate] = matrixsetup1P2(filename)  
+
+   
+   % All dependend on P (itineraries)
+    [~, ditinerary.flightnr]    = xlsread(filename,2,'F2:G738');   % flight numbers both legs
+    itinerary.flightnr          = ditinerary.flightnr; 
+    itinerary.paths             = xlsread(filename,2,'H2:H738'); % itinerary number
+    itinerary.fare              = xlsread(filename,2,'E2:E738'); % fare associated to itineraries
+    itinerary.demand            = xlsread(filename,2,'D2:D738'); % demand associated to itineraries
+    
+   % All dependend on L (flights)
+    [~, dflight.flightnr]       = xlsread(filename,1,'A2:A233'); % flight number 
+    flight.flightnr             = dflight.flightnr;
+    flight.capacity             = xlsread(filename,1,'F2:F233'); % capacity associated to existing arc
+    
+   % All about recapturing
+    recapture.rate     = xlsread(filename,3,'C2:C300'); % recapture rate associated to some arcs
+    recapture.p   = xlsread(filename,3,'G2:G300'); % recaptured from path p
+    recapture.r   = xlsread(filename,3,'H2:H300'); % recaptured to path r
+
+
+        %% EXAMPLE Output INITIALISATION!!!!!!!!!!!!!!!!!!!!
+   % What do we need
+    P = numel(itinerary.paths); 
+    L = numel(flight.capacity);  % ugly way of doing things
+    
+    fare         = itinerary.fare;    % Initially all can be recaptured to p=0, with fare_0 = 0
+    recap_p      = recapture.p ;
+    recap_r      = recapture.r ;
+    recaprate    = recapture.rate ;
+    pathflights  = itinerary.flightnr ;
+    flightnrs    = flight.flightnr;
+    demand       = itinerary.demand;
+    capacity     = flight.capacity;
+    
+    %% CALCULATIONS
+    Bpr = xlsread('Bpr',1,'A1:ABJ738') ;
+
+    Rcol = 738;
+    Pcol = 1:P;
+    col = zeros(P,2);
+    for r = Rcol
+        for p = Pcol
+            col(p,:) = [p r];
+        end
+    end
+
+
+    % The binary value: Delta
+    delta = cell(P,1); % For each Path all Flights are checked: delta{p,1}(i)
+    for p = 1:P
+        delta{p,1} = zeros(L,1);
+        for i = 1:L
+            if isequal(pathflights(p,1), flightnrs(i)) || isequal(pathflights(p,2), flightnrs(i))
+                delta{p,1}(i) = 1;            
+            end
+        end
+    end
+
+    delta{738,1} = zeros(L,1);
+
+    % The daily unconstrained demand on flight(i): Q
+    Q = zeros(L,1);
+    for i = 1:L
+        a = zeros(P,1);
+        for p = 1:P
+            a(p) = demand(p)*delta{p,1}(i);
+        end
+        Q(i) = sum(a);
+    end
+
+    % adding the ficticious fare and element
+    fare_r = [fare;0] ; % adding the ficticious recapture fare
+    R = P +1;
+
+
+    costfull = zeros(P,R);      % full cost matrix
+    for p = 1:P
+        for r = 1:R
+            costfull(p,r) = fare(p) - Bpr(p,r)*fare_r(r);
+        end
+    end
+
+end
+%% Write Bpr file
+% ptor = zeros(P);
+% for p = 1:P
+%     for r = 1:P
+%         for laa = 1:numel(recap_p)
+%             if p == recap_p(laa) && r == recap_r(laa)
+%                 ptor(p,r) = recaprate(laa);
+%             end
+%         end
+%     end
+% end
+% 
+% xlswrite('Bpr.xlsx',ptor)
+
+
+
+
+
+
+
+    
+
